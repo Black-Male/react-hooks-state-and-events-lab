3d526070a59950ba8977db00679bdb6dffcf53a2
Black-Male <Gkaranja20@gmail.com>
2023-01-14 13:16:46 +0300

    Work Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  //const[val, setVal] = useState(false)
+  const [toggle, isToggling] = useState(false)
 
+  const appClass = toggle ? "App dark" : "App light"
+
+
+  let btnDown = () => {
+    console.log(toggle)
+    isToggling((z) => !z)
+  }
+  // const guess = () => {
+  //   setVal((val) => !val)
+  // }
+
+  
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={btnDown}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [modified, isModified] = useState('Add to cart')
+  const [styler, isStyled] = useState("")
+
+  const btnDown =() => {
+    if(modified === 'Add to cart'){
+      isModified('Remove from cart')
+    }else{
+      isModified('Add to cart')
+    }
+
+    if (styler === ""){
+      isStyled("in-cart")
+    }else{
+      isStyled("")
+    }
+  }
+
   return (
-    <li className="">
+    <li className={styler}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={btnDown} className="add">{modified}</button>
     </li>
   );
 }
